e2f20ac4ccf9aa4ceb1eaf7d0b4d585ab2dbf515
Tom <red.shirt083@passinbox.com>
2026-02-11 10:27:54 -0800

    Added Get User by ID.

--- src/Project498.WebApi/Controllers/UsersController.cs
+++ src/Project498.WebApi/Controllers/UsersController.cs
@@ -22,12 +22,25 @@ public class UsersController : ControllerBase
         return await _context.Users.ToListAsync();
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+
     [HttpPost]
     public async Task<ActionResult<User>> AddUser(User user)
     {
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
-        return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, user);
+        return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
     }
 
     [HttpPut("{id}")]
